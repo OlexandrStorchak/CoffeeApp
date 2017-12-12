143e54496b84caeb40307509ab78b57436fd835b
St <new15@ukr.net>
2017-12-12 12:21:26 +0200

    add and reuse fragment

--- app/src/main/java/com/example/st/coffeeapp/mainActivity/MainActivity.java
+++ app/src/main/java/com/example/st/coffeeapp/mainActivity/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.st.coffeeapp.mainActivity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.st.coffeeapp.R;
 import com.example.st.coffeeapp.menuFragment.menuFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainActivityInterface {
+
+    public static final String MENU_FRAGMENT_TAG = "menu";
+    public static final String TAG = "tag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getSupportFragmentManager().beginTransaction()
-                .add(R.id.mainframe, new menuFragment(), "menu").commit();
+        showMenuFragment();
 
 
+    }
+
+    @Override
+    public void showMenuFragment() {
+        if (getSupportFragmentManager().findFragmentByTag(MENU_FRAGMENT_TAG) == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.mainframe, new menuFragment(), MENU_FRAGMENT_TAG).commit();
+
+        } else {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.mainframe, getSupportFragmentManager()
+                            .findFragmentByTag(MENU_FRAGMENT_TAG)).commit();
+
+        }
     }
 }
